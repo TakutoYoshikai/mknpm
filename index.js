3c9e0ab3b3ea2b85028f7e500f9d9b3559725bf2
lucky <takuto.yoshikai@gmail.com>
2021-10-24 17:12:55 +0900

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+function updatePackageJson(filePath, name) {
+  const packageJson = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  packageJson.name = name;
+  packageJson.bin = {}
+  packageJson.bin[name] = "./bin/" + name;
+  fs.writeFileSync(filePath, JSON.stringify(packageJson, null, "  "));
+}
+
+updatePackageJson(process.argv[2], process.argv[3]);
